5757f9d559b3048dfca2149e284c3e744efdd611
Logan Gore <gorel@purdue.edu>
2015-09-21 13:51:53 -0400

    Most of the login code finished

---
+++ dli_app/mod_auth/forms.py
@@ -0,0 +1,88 @@
+from flask.ext.wtf import (
+    Form,
+)
+
+from wtforms import (
+    PasswordField,
+    SelectField,
+    TextField,
+    validators,
+)
+
+class RegistrationForm(Form):
+    email = TextField(
+        'Email',
+        validators=[
+            validators.Email(),
+            validators.Required(
+                message='You must provide your work email address.',
+            ),
+        ],
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[
+            validators.Required(
+                message='Please enter a password.',
+            ),
+            validators.EqualTo(
+                'confirm_password',
+                message='Passwords must match',
+            ),
+        ],
+    )
+
+    confirm_password = PasswordField(
+        'Confirm Password',
+        validators=[
+            validators.Required(
+                message='Please confirm your password.',
+            ),
+        ],
+    )
+
+    location = SelectField(
+        'Location',
+        validators=[
+            validators.Required(
+                message='Please enter your default location.',
+            ),
+        ],
+        coerce=int,
+    )
+
+class LoginForm(Form):
+    email = TextField(
+        'Email',
+        validators=[
+            validators.Email(),
+            validators.Required(
+                message='You must provide your work email address.',
+            ),
+        ],
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[
+            validators.Required(
+                message='Please enter a password.',
+            ),
+        ],
+    )
+
+    remember = BooleanField(
+        'Remember Me?',
+    )
+
+class ForgotForm(Form):
+    email = TextField(
+        'Email',
+        validators=[
+            validators.Email(),
+            validators.Required(
+                message='You must provide your work email address.',
+            ),
+        ],
+    )
