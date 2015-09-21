63bbbc6a60e1e16ed31a196c05853d67ec120705
Logan Gore <gorel@purdue.edu>
2015-09-21 13:51:53 -0400

    Most of the login code finished

--- dli_app/mod_auth/controllers.py
+++ dli_app/mod_auth/controllers.py
@@ -22,12 +22,16 @@ from werkzeug import (
 from dli_app import db, login_manager
 
 # Import forms
-#from dli_app.mod_auth.forms import (
-#)
+from dli_app.mod_auth.forms import (
+    LoginForm,
+    RegistrationForm,
+)
 
 # Import models
-#from dli_app.mod_auth.models import (
-#)
+from dli_app.mod_auth.models import (
+    Location,
+    User,
+)
 
 # Create a blueprint for this module
 mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
@@ -35,25 +39,62 @@ mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
 # Set all routing for the module
 @mod_auth.route('/register/', methods=['GET', 'POST'])
 def register():
-    if request.method == 'GET':
-        return render_template('auth/register.html')
+    # TODO: Only allow registration from an approved email link
+    form = RegistrationForm(request.form)
+    if form.validate_on_submit():
+        # TODO: Lots of error checking for unique values
+
+        location = Location.get(form.location.data)
+        if location is None:
+            flash('Invalid location.', 'alert-warning')
+            # TODO: Keep everything else populated somehow
+            return redirect(url_for('auth.register'))
+
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            password=form.password.data,
+            location=location,
+        )
+        db.session.add(user)
+
+        db.session.commit()
+
+        # Log the user in and redirect to the homepage
+        login_user(user)
+        return redirect(request.args.get('next') or url_for('default.home'))
     else:
-        # TODO: Register the new user and log them in
-        return redirect(request.args.get('next') or url_for('/'))
+        form.location.choices = [
+            (location.id, location.name) for location in Location.query.all()
+        ]
+        return render_template('auth/register.html', form=form)
 
 @mod_auth.route('/login/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
-        return render_template('auth/login.html')
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = User.get_by_email(form.email.data)
+
+        if user is None:
+            flash('No account with that email exists!', 'alert-warning')
+            return redirect(url_for('auth.login'))
+
+        if not user.check_password(form.password.data):
+            flash('Incorrect password! Try again?', 'alert-warning')
+            # TODO: Prepopulate email field somehow
+            return redirect(url_for('auth.login'))
+
+        # User has authenticated. Log in.
+        login_user(user, remember=form.remember.data)
+        return redirect(request.args.get('next') or url_for('default.home'))
     else:
-        # TODO: Login code here
-        # 1. Get form fields
-        # 2. Check password
-        # 3. Login
-        return redirect(request.args.get('next') or  url_for('/'))
+        return render_template('auth/login.html', form=form)
 
 @mod_auth.route('/logout/', methods=['POST'])
-@login_required
 def logout():
-    logout_user()
+    # Don't set login required, because it will send the user to the login page
+    # before redirecting them to logout. It doesn't make much sense to the user.
+    if current_user.is_authenticated:
+        logout_user()
+    flash('You have successfully logged out.', 'alert-success')
     return redirect(url_for('/'))
