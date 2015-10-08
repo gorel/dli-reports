bf78dc295bb19ac327e6389437305d8f4ab30779
Logan Gore <gorel@purdue.edu>
2015-10-08 10:25:23 -0400

    Simple wiki functional

--- dli_app/mod_wiki/controllers.py
+++ dli_app/mod_wiki/controllers.py
@@ -4,14 +4,50 @@ Author: Logan Gore
 This file is responsible for loading all site pages under /wiki.
 """
 
+from markdown import (
+    Markdown,
+)
+
+from markdown.extensions import (
+    extra,
+    nl2br,
+    toc,
+    wikilinks,
+)
+
 from flask import (
     Blueprint,
+    flash,
+    redirect,
     render_template,
+    url_for,
+)
+
+from flask_login import (
+    login_required,
+)
+
+from dli_app import (
+    db,
+    flash_form_errors,
 )
 
 from dli_app.mod_wiki.models import (
     WikiPage,
 )
+
+from dli_app.mod_wiki.forms import (
+    EditWikiPageForm,
+)
+
+EXTENSIONS = [
+    extra.ExtraExtension(),
+    nl2br.Nl2BrExtension(),
+    toc.TocExtension(),
+    wikilinks.WikiLinkExtension(base_url='/wiki/'),
+]
+
+MD = Markdown(extensions=EXTENSIONS)
 
 # Create a blueprint for this module
 mod_wiki = Blueprint('wiki', __name__, url_prefix='/wiki')
@@ -19,16 +55,81 @@ mod_wiki = Blueprint('wiki', __name__, url_prefix='/wiki')
 
 # Set all routing for the module
 @mod_wiki.route('/', methods=['GET'])
+@mod_wiki.route('/home/', methods=['GET'])
 def home():
     """Render the wiki homepage"""
-    return render_template('wiki/home.html')
+    page = WikiPage.query.filter_by(name='index').first()
+    html = ''
+
+    if page is not None:
+        html = MD.convert(page.content)
+
+    return render_template('wiki/home.html', html=html)
 
 
-@mod_wiki.route('/<page_name>', methods=['GET', 'POST'])
+@mod_wiki.route('/<page_name>/', methods=['GET'])
 def view_page(page_name):
     """ Render a specific page of the wiki"""
     page = WikiPage.query.filter_by(name=page_name).first()
     if page is None:
         return render_template('wiki/404.html'), 404
 
-    return render_template('wiki/view.html', page=page)
+    html = MD.convert(page.content)
+    return render_template('wiki/view.html', page=page, html=html, toc=MD.toc)
+
+
+@mod_wiki.route('/edit/', methods=['GET', 'POST'])
+@mod_wiki.route('/edit/<page_name>/', methods=['GET', 'POST'])
+@login_required
+def edit_page(page_name=''):
+    """Edit a specific page of the wiki"""
+    page = WikiPage.query.filter_by(name=page_name).first()
+
+    form = EditWikiPageForm()
+    if form.validate_on_submit():
+        # Update the WikiPage's information
+        if page is not None:
+            page.name = form.page.name
+            page.content = form.page.content
+            # TODO: Additional fields that need to be set here
+            flash(
+                "WikiPage updated successfully",
+                "alert-success",
+            )
+        # Create a new WikiPage
+        else:
+            db.session.add(page)
+            flash(
+                "WikiPage added successfully",
+                "alert-success",
+            )
+
+        db.session.commit()
+        return redirect(url_for('wiki.view_page', page_name=form.page.name))
+    else:
+        flash_form_errors(form)
+        if page is not None:
+            form.name.data = page.name
+            form.content.data = page.content
+        return render_template('wiki/edit.html', form=form)
+
+
+@mod_wiki.route('/delete/<int:page_id>/', methods=['POST'])
+@login_required
+def delete_page(page_id):
+    """Delete a given WikiPage"""
+    page = WikiPage.query.get(page_id)
+    if page is None:
+        flash(
+            "WikiPage not found!",
+            "alert-warning",
+        )
+    else:
+        flash(
+            "WikiPage deleted successfully",
+            "alert-success",
+        )
+        db.session.delete(page)
+        db.session.commit()
+
+    return redirect(url_for('wiki.home'))
