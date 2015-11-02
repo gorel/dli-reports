abb15517a5841a6421572794cddc52524e73f311
Evan Arnold <earnold1993@gmail.com>
2015-11-01 19:10:10 -0500

    Added edit chart functionality

--- dli_app/mod_reports/models.py
+++ dli_app/mod_reports/models.py
@@ -47,7 +47,7 @@ class Report(db.Model):
     __tablename__ = "report"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
-    name = db.Column(db.String(64))
+    name = db.Column(db.String(128))
     fields = db.relationship(
         'Field',
         secondary=report_fields,
@@ -311,7 +311,7 @@ class Chart(db.Model):
     """Model for a DLI Chart"""
     __tablename__ = "chart"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), index=True)
+    name = db.Column(db.String(128), index=True)
     with_table = db.Column(db.Boolean)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
     ctype_id = db.Column(db.Integer, db.ForeignKey("chart_type.id"))
