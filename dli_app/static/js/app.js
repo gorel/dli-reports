c0166cf80c38356af04be654b1c0f8950eceecea
Jon Egeland <audiobahn404@gmail.com>
2015-11-14 17:54:36 -0500

    No more forms for POST requests!

--- dli_app/static/js/app.js
+++ dli_app/static/js/app.js
@@ -4,4 +4,24 @@ $(function() {
     format: 'yyyy-mm-dd',
     autoclose: 'true'
   });
+
+  $('a[data-method]').click(function(e) {
+    method = $(this).data('method');
+    action = $(this).attr('href');
+    csrf   = $('meta[name=csrf-token]').attr('content');
+
+    // Allow GET requests to go through normally
+    if(method == 'GET') return true;
+    // Prevent all other requests from executing
+    e.preventDefault();
+
+    // Create a form object with the appropriate method and action
+    var form = $('<form></form>');
+    form.attr('method', method);
+    form.attr('action', action);
+    form.append('<input type="hidden" name="csrf_token" value="'+csrf+'" />');
+
+    $(document.body).append(form);
+    form.submit();
+  });
 });
