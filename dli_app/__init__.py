66527566c65aba0775b1b381c93240fdfd0297c8
Logan Gore <gorel@purdue.edu>
2015-12-08 16:41:07 -0500

    Update __init__.py

--- dli_app/__init__.py
+++ dli_app/__init__.py
@@ -21,15 +21,17 @@ from flask_wtf.csrf import CsrfProtect
 # Other imports
 from htmlmin.main import minify
 
-# Check for environment variables.  Exit if they are not set properly
+
 ENVIRON_KEYS = [
     'DLI_REPORTS_GITHUB_USERNAME', 
     'DLI_REPORTS_GITHUB_PASSWORD', 
     'DLI_REPORTS_ADMIN_PASSWORD', 
     'DLI_REPORTS_SITE_URL', 
     'DLI_REPORTS_DEV_EMAIL', 
-    'DLI_REPORTS_GITHUB_ISSUES_URL'
+    'DLI_REPORTS_GITHUB_ISSUES_URL',
 ]
+
+# Check for environment variables.  Exit if they are not set properly
 for key in ENVIRON_KEYS:
     if not key in os.environ:
         sys.stdout.write('Error! Environment variables not set up properly.')
